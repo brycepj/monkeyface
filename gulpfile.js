9c3dd4bbfc1e4abfc25a276f63ead9175bee6b7f
Bryce Johnson <brycepj@gmail.com>
2016-04-18 12:52:59 -0400

    Set up tasks for building and testing in gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var ts = require('gulp-typescript');
 
 gulp.task('test-cfg', () => {
   var cfg_path = './test/_config.js';
@@ -19,9 +20,32 @@ gulp.task('test-integration', () => {
     .pipe(mocha());
 });
 
+gulp.task('log-perf', () => {
+  // run all scripts, diff results against previous, log most recent, perhaps log stats.
+
+});
+
 gulp.task('test', ['test-cfg', 'test-units', 'test-integration']);
 
 gulp.task('test-dev', ['test-cfg', 'test-units', 'test-integration'], () => {
   gulp.watch('./**/*.js', ['test-dev']);
 });
+
+var tsProject = ts.createProject('tsconfig.json', {});
+var ts_files = ['lib/**/*.ts'];
+
+gulp.task('dev-ts', ['build-ts'], () => {
+  gulp.watch(ts_files, ['test-dev']);
+});
+
+gulp.task('build-ts', () => {
+  return gulp.src(ts_files)
+    .pipe(ts(tsProject))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', ['build-ts','test']);
+
+// move built js files to dist.
+// update refs
 
